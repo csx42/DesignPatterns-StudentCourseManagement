1c6c1e8feac0973a2db152e9f43f2c9b1e4dc64f
Spoorthi Sanjay <spoorthisanjay@Spoorthis-Air.lan>
2023-09-19 15:37:53 -0400

    add file write classes

--- studentCoursesMgmt/src/studentCoursesMgmt/util/FileDisplayInterface.java
+++ studentCoursesMgmt/src/studentCoursesMgmt/util/FileDisplayInterface.java
@@ -1,5 +1,11 @@
 package studentCoursesMgmt.util;
 
+import java.io.IOException;
+
 public interface FileDisplayInterface {
+
+    public void getFileForWrite()throws IOException;
+    public void printOutputToFile(String output)throws IOException;
+    public void closeFileWriter() throws IOException;
 	
 }
--- studentCoursesMgmt/src/studentCoursesMgmt/util/Results.java
+++ studentCoursesMgmt/src/studentCoursesMgmt/util/Results.java
@@ -1,5 +1,34 @@
 package studentCoursesMgmt.util;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Results implements FileDisplayInterface, StdoutDisplayInterface {
-	
+	private String filePath;
+    FileProcessor fileProcessor;
+
+    public Results(String filePath) {
+        this.filePath = filePath;
+        fileProcessor = new FileProcessor(filePath);
+    }
+
+    public void getFileForWrite() throws IOException {
+        fileProcessor.getFileForWrite();
+    }
+
+    public void printOutputToFile(String output) throws IOException {
+        fileProcessor.writeToFile(output);
+    }
+
+    public void closeFileWriter() throws IOException{
+        fileProcessor.closeFile();
+    }
+
+    public void printOutputToStdout(){
+
+    }
+
+    public void printErrorToStdout(){
+
+    }
 }
