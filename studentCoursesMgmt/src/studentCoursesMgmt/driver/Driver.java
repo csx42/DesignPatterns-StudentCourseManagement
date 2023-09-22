8d0dd5fee51bf4f29aa077ffe7aefdb1a6f8dac9
Spoorthi Sanjay <spoorthisanjay@Spoorthis-Air.lan>
2023-09-21 21:41:52 -0400

    print function modification

--- studentCoursesMgmt/src/studentCoursesMgmt/driver/Driver.java
+++ studentCoursesMgmt/src/studentCoursesMgmt/driver/Driver.java
@@ -1,34 +1,24 @@
 package studentCoursesMgmt.driver;
 
-import studentCoursesMgmt.util.FileDisplayInterface;
 import studentCoursesMgmt.util.FileInput;
-import studentCoursesMgmt.util.Results;
-
 import java.util.Arrays;
 import java.io.IOException;
 
 /**
- * @author placeholder
+ * @author spoorthi
  *
  */
 public class Driver {
 	public static void main(String[] args) throws IOException {
 
-		/*
-		 * As the build.xml specifies the arguments as argX, in case the
-		 * argument value is not given java takes the default value specified in
-		 * build.xml. To avoid that, below condition is used
-		 */
+	    if (args.length != 5 || args[0].equals("${arg0}") || args[1].equals("${arg1}") || args[2].equals("${arg2}")
+				|| args[3].equals("${arg3}") || args[4].equals("${arg4}")) {
 
-//	    if (args.length != 5 || args[0].equals("${arg0}") || args[1].equals("${arg1}") || args[2].equals("${arg2}")
-//				|| args[3].equals("${arg3}") || args[4].equals("${arg4}")) {
-//
-//			System.err.println("Error: Incorrect number of arguments. Program accepts 5 arguments.");
-//			System.exit(0);
-//		}
+			System.err.println("Error: Incorrect number of arguments. Program accepts 5 arguments.");
+			System.exit(0);
+		}
 
 		Course[] availableCourseList = readCourseFile();
-
 		StudentCourseInterface studentCourseAllocation = null;
 		String[] preferred = new String[0];
 		allocateAndPrintResult(studentCourseAllocation, preferred, availableCourseList);
@@ -47,10 +37,7 @@ public class Driver {
 				preferred[8] = preferred[8].substring(0, 1);
 				studentCourseAllocation.setPreferredCourses(preferred);
 				availableCourseList = studentCourseAllocation.allocateCourses(availableCourseList);
-				FileDisplayInterface print = new Results("/Users/spoorthisanjay/DP/registration_results.txt");
-				print.getFileForWrite();
-				studentCourseAllocation.printResults(print);
-				print.closeFileWriter();
+				studentCourseAllocation.printResults("/Users/spoorthisanjay/DP/registration_results.txt");
 			}
 		} while (input != null);
 	}
